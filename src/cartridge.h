0c8c903731bea9f03e422f428bf9d6dff54b5b83
brunozer09 <levybruno03@gmail.com>
2026-01-14 08:57:18 -0300

    Cartridge header parsing

---
+++ src/cartridge.h
@@ -0,0 +1,24 @@
+#ifndef CARTRIDGE_H
+#define CARTRIDGE_H
+
+#include <stdint.h>
+
+typedef struct {
+  uint8_t entry_point[4]; 
+  uint8_t nintendo_logo[48];
+  unsigned char title[16];
+  bool cgb_flag;
+  unsigned char new_licensee_code[2];
+  bool sgb_flag;
+  uint8_t cartridge_type;
+  uint8_t rom_size;
+  uint8_t ram_size;
+  uint8_t destination_code;
+  uint8_t
+} Header;
+
+typedef struct{
+  Header header;
+} Cartridge;
+
+#endif  
